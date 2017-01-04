53daf8ed90c1fa189a463792692919cce1e5d5f9
Alexey <alexl5000@yandex.ru>
2017-01-04 19:33:19 +0200

    Add files via upload

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            Console.WriteLine("EXE path: {0}", path);
+
+            string cfgpath = path + "time.cfg";
+            Console.WriteLine("CFG path: {0}", cfgpath);
+
+            const string quote = "\"";
+
+            while (true)
+            {
+
+                StringBuilder sb = new StringBuilder();
+
+                File.WriteAllText(cfgpath, string.Empty);
+
+                sb.Append("alias currenttime " + quote + "echo " + quote + "Current time: " + DateTime.Now.ToString("HH:mm:ss") + quote + quote + Environment.NewLine);
+                File.WriteAllText(cfgpath, sb.ToString());
+
+                sb.Clear();
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
+    
